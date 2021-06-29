b9496ca3054288f0e05ff291a1e70859604dcfe5
Manno <si.jorgelucas@gmail.com>
2021-06-29 15:49:04 -0300

    Executável

---
+++ src/br/estacio/exec/MeuApp.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.exec;
+
+import br.estacio.controle.ControleLogin;
+import br.estacio.visao.TelaLogin;
+
+public class MeuApp {
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            
+            //Iniciar o formulario
+            ControleLogin controle  = new ControleLogin();
+            
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+    }
+}
